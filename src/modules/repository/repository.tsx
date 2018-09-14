bf382c615610bc2e29c1e019f7678ff24d5b7e11
ricardo <rikiricardo16@hotmail.com>
2018-09-13 17:16:19 -0800

    configuration module repository

---
+++ src/modules/repository/repository.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+class Repository extends React.Component {
+
+    public render() {
+        return (
+            <div className="Login">
+                <p>repositories</p>
+            </div>
+        );
+    }
+}
+
+export default Repository;
